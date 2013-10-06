a7bf668cd15aba62c891ac5f3c43cc06e524a0c9
Diego Williams Aguilar Montaño <alu0100592368@ull.edu.es>
2013-10-06 02:05:58 +0100

    arreglando identacion 2

--- suma.rb
+++ suma.rb
@@ -6,18 +6,18 @@ c = Array.new                             #matriz de filas c
 m, n = a.size, a[1].size                  #m son las filas y n las columnas
 
 for i in 0...m do                         #iterador sobre m filas
-	c[i] = Array.new                       #la 2da dimension (columnas) de c
-	for j in 0...n do                      #iterador sobre columnas
-		c[i][j] = 0                         #se inicializan en 0
-	end
+   c[i] = Array.new                       #la 2da dimension (columnas) de c
+   for j in 0...n do                      #iterador sobre columnas
+      c[i][j] = 0                         #se inicializan en 0
+   end
 end
 
 for i in 0...m do                         #iteramos de nuevo sobre las filas
-	for j in 0...n do                      #por cada fila, se iteran columnas
-		c[i][j] = a[i][j] + b[i][j]         #se calcula la suma
-	end
+   for j in 0...n do                      #por cada fila, se iteran columnas
+      c[i][j] = a[i][j] + b[i][j]         #se calcula la suma
+   end
 end
 
 c.each do |fila|                          #por cada fila
-	puts fila.join(" ")                    #imprime el resultado
+   puts fila.join(" ")                    #imprime el resultado
 end
