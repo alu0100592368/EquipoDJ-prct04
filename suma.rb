3a3559789d1d2508168b3dc655bdb21614059882
Diego Williams Aguilar Montaño <alu0100592368@ull.edu.es>
2013-10-06 02:03:04 +0100

    arreglando identacion

--- suma.rb
+++ suma.rb
@@ -1,26 +1,23 @@
 #Suma de dos matrices cuadradas A y B dadas
 
-a = [[3, 0, 2], [5, 1, 6], [4, 3, 1]]		#matriz a
+a = [[3, 0, 2], [5, 1, 6], [4, 3, 1]]     #matriz a
+b = [[1, 4, 2], [4, 3, 3], [1, 3, 6]]     #matriz b
+c = Array.new                             #matriz de filas c
+m, n = a.size, a[1].size                  #m son las filas y n las columnas
 
-b = [[1, 4, 2], [4, 3, 3], [1, 3, 6]]		#matriz b
-
-c = Array.new 										#matriz de filas c
-
-m, n = a.size, a[1].size						#m son las filas y n las columnas
-
-for i in 0...m do									#iterador sobre m filas
-	c[i] = Array.new								#la 2da dimension (columnas) de c
-	for j in 0...n do 							#iterador sobre columnas
-		c[i][j] = 0									#se inicializan en 0
+for i in 0...m do                         #iterador sobre m filas
+	c[i] = Array.new                       #la 2da dimension (columnas) de c
+	for j in 0...n do                      #iterador sobre columnas
+		c[i][j] = 0                         #se inicializan en 0
 	end
 end
 
-for i in 0...m do 								#iteramos de nuevo sobre las filas
-	for j in 0...n do 							#por cada fila, se iteran columnas
-		c[i][j] = a[i][j] + b[i][j]			#se calcula la suma
+for i in 0...m do                         #iteramos de nuevo sobre las filas
+	for j in 0...n do                      #por cada fila, se iteran columnas
+		c[i][j] = a[i][j] + b[i][j]         #se calcula la suma
 	end
 end
 
-c.each do |fila|									#por cada fila
-	puts fila.join(" ")							#imprime el resultado
+c.each do |fila|                          #por cada fila
+	puts fila.join(" ")                    #imprime el resultado
 end
