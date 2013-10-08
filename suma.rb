f5d3b93809a9743bbc5463c6130dfb875febad7a
Javier Mena <alu0100454741@ull.edu.es>
2013-10-08 09:49:52 +0100

    realizando cambios para mezcla

--- suma.rb
+++ suma.rb
@@ -2,19 +2,20 @@
 
 a = [[3, 0, 2], [5, 1, 6], [4, 3, 1]]     #matriz a
 b = [[1, 4, 2], [4, 3, 3], [1, 3, 6]]     #matriz b
-c = Array.new				   #matriz de filas c
-d = Array.new
+c = Array.new(3, Array.new(3,0))			   #matriz de filas c
+puts c
+d = Array.new(3, Array.new(3,0))
 m, n = a.size, a[1].size                  #m son las filas y n las columnas
 
 
-for i in 0...m do                         #iterador sobre m filas
-   c[i] = Array.new                       #la 2da dimension (columnas) de c
-   d[i] = Array.new
-   for j in 0...n do                      #iterador sobre columnas
-      c[i][j] = 0                         #se inicializan en 0
-      d[i][j] = 0
-   end
-end
+# for i in 0...m do                         #iterador sobre m filas
+#    c[i] = Array.new                       #la 2da dimension (columnas) de c
+#    d[i] = Array.new
+#    for j in 0...n do                      #iterador sobre columnas
+#       c[i][j] = 0                         #se inicializan en 0
+#       d[i][j] = 0
+#    end
+# end
 
 
 
@@ -36,11 +37,11 @@ end
 puts"\n"
 
 puts "MATRIZ B"
-b.each do |fila|                          #por cada fila
+a.each do |fila|                          #por cada fila
    puts fila.join(" ")             #imprime el resultado
 end
 
-puts "\n"
+
 
 puts "SUMA DE MATRICES"
 c.each do |fila|                          #por cada fila
