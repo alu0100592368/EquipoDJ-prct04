ced799dd5c56c712f182de528600b9e267e9484e
Javier Mena <alu0100454741@ull.edu.es>
2013-10-08 09:53:05 +0100

    ultimos cambios

--- suma.rb
+++ suma.rb
@@ -2,20 +2,20 @@
 
 a = [[3, 0, 2], [5, 1, 6], [4, 3, 1]]     #matriz a
 b = [[1, 4, 2], [4, 3, 3], [1, 3, 6]]     #matriz b
-c = Array.new(3, Array.new(3,0))			   #matriz de filas c
-puts c
-d = Array.new(3, Array.new(3,0))
+#c = Array.new(3, Array.new(3,0))			   #matriz de filas c
+#puts c
+#d = Array.new(3, Array.new(3,0))
 m, n = a.size, a[1].size                  #m son las filas y n las columnas
 
 
-# for i in 0...m do                         #iterador sobre m filas
-#    c[i] = Array.new                       #la 2da dimension (columnas) de c
-#    d[i] = Array.new
-#    for j in 0...n do                      #iterador sobre columnas
-#       c[i][j] = 0                         #se inicializan en 0
-#       d[i][j] = 0
-#    end
-# end
+ for i in 0...m do                         #iterador sobre m filas
+    c[i] = Array.new                       #la 2da dimension (columnas) de c
+    d[i] = Array.new
+    for j in 0...n do                      #iterador sobre columnas
+       c[i][j] = 0                         #se inicializan en 0
+       d[i][j] = 0
+    end
+ end
 
 
 
